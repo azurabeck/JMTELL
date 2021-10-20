c547ae90fa836d7bb41ffcdcec8063cad3581190
Rebecca Souza <rebecca@psafe.com>
2021-10-20 13:00:30 -0300

    fix zindex

--- src/web_pages/organism/Navbar/menuData.js
+++ src/web_pages/organism/Navbar/menuData.js
@@ -1,7 +1,3 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
-
 export const MenuData = [
     {
         title: 'Home',
