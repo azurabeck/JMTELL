396728fa0296510a831523f39975e34ec7183fbd
Rebecca Souza <rebecca@psafe.com>
2021-11-06 19:00:09 -0300

    spotlight inside home

--- src/web_pages/pages/home/About/index.js
+++ src/web_pages/pages/home/About/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './style.scss';
 
+import EFFECT from '../../../atoms/SVG/effect.svg'
+
 
 const Header = () => {
 
     return (
-        <div className='about'>
+        <div className='about' style={{background: `url(${EFFECT})`}}>
             <div className='bg-img'></div>
             <div className='title'>QUEM SOMOS?</div>
             <div className='desc'>Somos uma empresa distribuidora dos mais avançados sistemas de telefonia, 
--- src/web_pages/pages/home/Products/index.js
+++ src/web_pages/pages/home/Products/index.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React , { useEffect , useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCaretRight , faCaretLeft } from '@fortawesome/free-solid-svg-icons'
 import ButtonStroke from '../../../atoms/BUTTON_STROKE'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 import './style.scss';
 
 
-const Header = () => {
+const Header = (props) => {
+
+    
+    const PRODUCTS_DATA = props.products
+    const [ SPOTLIGHT , updateProducts] = useState() 
+
+    useEffect(() => {
+        async function anyNameFunction() {
+            const PRODUCTS_SPOTLIGHT = await PRODUCTS_DATA && PRODUCTS_DATA.filter(item => item.spotlight === true)
+            updateProducts( PRODUCTS_SPOTLIGHT && PRODUCTS_SPOTLIGHT)
+        }
+        anyNameFunction();
+    }, [PRODUCTS_DATA]);
+        
 
     return (
         <div className='products'>
@@ -15,62 +31,48 @@ const Header = () => {
 
             <div className='products-spotlight'>
                 <FontAwesomeIcon icon={faCaretLeft} className='arrow' />
-                <div className='product'>
-                    <img className='product-img' alt='' src='https://m.media-amazon.com/images/I/51alwW2xKtL._AC_SL1000_.jpg'/>
-                    <div className='product-title'>VHD 3120 B</div>
-                    <div className='product-desc'>Intelbras Multi HD®</div>
-                    <div className='btn'>
-                        <ButtonStroke 
-                            TEXT='Saiba mais'
-                            WIDTH='160px'
-                            HEIGHT='35px'
-                            BTN_TYPE={3}
-                            TO='/products'/>
-                    </div>
-                </div>
-                <div className='product'>
-                    <img className='product-img' alt='' src='https://m.media-amazon.com/images/I/51alwW2xKtL._AC_SL1000_.jpg'/>
-                    <div className='product-title'>VHD 3120 B</div>
-                    <div className='product-desc'>Intelbras Multi HD®</div>
-                    <div className='btn'>
-                        <ButtonStroke 
-                            TEXT='Saiba mais'
-                            WIDTH='160px'
-                            HEIGHT='35px'
-                            BTN_TYPE={3}
-                            TO='/products'/>
-                    </div>
-                </div>
-                <div className='product'>
-                    <img className='product-img' alt='' src='https://m.media-amazon.com/images/I/51alwW2xKtL._AC_SL1000_.jpg'/>
-                    <div className='product-title'>VHD 3120 B</div>
-                    <div className='product-desc'>Intelbras Multi HD®</div>
-                    <div className='btn'>
-                        <ButtonStroke 
-                            TEXT='Saiba mais'
-                            WIDTH='160px'
-                            HEIGHT='35px'
-                            BTN_TYPE={3}
-                            TO='/products'/>
-                    </div>
-                </div>
-                <div className='product'>
-                    <img className='product-img' alt='' src='https://m.media-amazon.com/images/I/51alwW2xKtL._AC_SL1000_.jpg'/>
-                    <div className='product-title'>VHD 3120 B</div>
-                    <div className='product-desc'>Intelbras Multi HD®</div>
-                    <div className='btn'>
-                        <ButtonStroke 
-                            TEXT='Saiba mais'
-                            WIDTH='160px'
-                            HEIGHT='35px'
-                            BTN_TYPE={3}
-                            TO='/products'/>
-                    </div>
-                </div>
+
+
+                {
+                    SPOTLIGHT && SPOTLIGHT.slice(0,5).map((item, index) => {
+                        return (
+                            <div className='product'>
+                                <div className='product-img' > <img alt='' src={item.img}/> </div>
+                                <div className='product-title'>{item.name}</div>
+                                <div className='product-desc'>{item.model}</div>
+                                <div className='btn'>
+                                    <ButtonStroke 
+                                        TEXT='Saiba mais'
+                                        WIDTH='160px'
+                                        HEIGHT='35px'
+                                        BTN_TYPE={3}
+                                        TO='/products'/>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+
+               
+               
+
+
                 <FontAwesomeIcon icon={faCaretRight} className='arrow' />
             </div>
         </div>
     )
 }
 
-export default Header
+const mapStateToProps = (state) => {
+    return {
+        products: state.firestore.ordered.products
+    }
+  }
+  
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        { collection: 'products' }
+        
+    ])
+)(Header)
