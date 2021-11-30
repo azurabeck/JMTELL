f1a4c0bba8e0daa6fcfd8c2864de43ba4df91fc1
Rebecca Souza <rebecca@psafe.com>
2021-11-30 00:00:08 -0300

    fix

--- src/web_pages/organism/Footer/AssociatedMenu/index.js
+++ src/web_pages/organism/Footer/AssociatedMenu/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { Link } from "react-router-dom";
 import './style.scss';
@@ -15,10 +16,10 @@ const AssociatedMenu = () => {
                     <div className='title'>Distribuídores autorizados</div>
                     <div className='group-columns'>
                         <div className='column'>
-                            <Link to=''>    Intelbraz > acesse o site   </Link>
-                            <Link to=''>    Linear HSV > acesse o site  </Link>
-                            <Link to=''>    Prysmian > acesse o site    </Link>
-                            <Link to=''>    Peccinin > acesse o site    </Link>
+                            <a to='https://intelbras.com/pt-br/' target='_blank'>    Intelbras > acesse o site   </a>
+                            <a to='https://www.linear-hcs.com.br/' target='_blank'>     Linear HCS > acesse o site  </a>
+                            <a to='https://br.prysmiangroup.com/pt/company/the-group-history/local-history' target='_blank'>     Prysmian > acesse o site    </a>
+                            <a to='https://nice.com.br/peccinin/' target='_blank'>     Peccinin > acesse o site    </a>
                         </div>
                     </div>
                 </div>   
@@ -28,11 +29,13 @@ const AssociatedMenu = () => {
                         <div className='group-columns'>
                             <div className='column'>
                             <div className='desc'> R. Barão de Melgaço, 880, Cordovil, Rio de Janeiro – RJ 21250-320
-                                <a href='#'> contato@jmtell.com.br </a> - (21) 3351-1211 </div>
+                                <a href="mailto:contato@jmtell.com.br"> contato@jmtell.com.br </a> - (21) 3351-1211 </div>
                             </div>
                             <div className='column row'>  
-                                <EMAIL />
-                                <WHATSAPP />
+                                <a href="mailto:contato@jmtell.com.br"> <EMAIL /> </a>
+                                <a href='https://api.whatsapp.com/send?l=pt_br&phone=55213351-1211' className='whatsapp' target='_blank' rel="noreferrer">
+                                    <WHATSAPP />
+                                </a>
                                 <BUTTON_STROKE
                                         TEXT='ACESSE O BLOCK'
                                         WIDTH='125px'
