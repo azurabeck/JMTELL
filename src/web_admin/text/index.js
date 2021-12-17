49db558c41ab174f3c4ca0d385f2243bee57eed4
Rebecca Souza <rebecca@psafe.com>
2021-12-17 12:36:38 -0300

    remove en option

--- src/web_admin/text/index.js
+++ src/web_admin/text/index.js
@@ -18,7 +18,7 @@ const Text = () => {
 
     return (
         <div className='text-admin'>   
-            <FastBar/>>
+            <FastBar/>
             
             <div className='text-title'> Edição dos textos </div>
             <div className='text-desc'> Selecione a página que gostaria de alterar o texto </div>
@@ -26,15 +26,10 @@ const Text = () => {
             <div className='text-group'>
 
                 <div className='btn-text' onClick={() => handleTextEdtion('home_pt')}>Home - PT</div>
-                <div className='btn-text' onClick={() => handleTextEdtion('home_en')}>Home - EN</div>
                 <div className='btn-text' onClick={() => handleTextEdtion('company_pt')}>Empresa - PT</div>
-                <div className='btn-text' onClick={() => handleTextEdtion('company_en')}>Empresa - EN</div>
                 <div className='btn-text' onClick={() => handleTextEdtion('product_pt')}>Produtos - PT</div>
-                <div className='btn-text' onClick={() => handleTextEdtion('product_en')}>Produtos - EN</div>
                 <div className='btn-text' onClick={() => handleTextEdtion('contact_pt')}>Contato - PT</div>
-                <div className='btn-text' onClick={() => handleTextEdtion('contact_en')}>Contato - EN</div>
                 <div className='btn-text' onClick={() => handleTextEdtion('blog_pt')}>Blog - PT</div>
-                <div className='btn-text' onClick={() => handleTextEdtion('blog_en')}>Blog - EN</div>
                
             </div>
 
@@ -42,15 +37,10 @@ const Text = () => {
                 <div className='edtion-area'>
                     
                     { textEdition && textEdition === 'home_pt' && <HOME isEditing='home_pt'/> }
-                    { textEdition && textEdition === 'home_en' && <HOME isEditing='home_pt'/> }
                     { textEdition && textEdition === 'product_pt' && <PRODUCT isEditing='product_pt'/> }
-                    { textEdition && textEdition === 'product_en' && <PRODUCT isEditing='product_en'/> }
                     { textEdition && textEdition === 'company_pt' && <COMPANY isEditing='company_pt'/> }
-                    { textEdition && textEdition === 'company_en' && <COMPANY isEditing='company_en'/> }
                     { textEdition && textEdition === 'contact_pt' && <CONTACT isEditing='contact_pt'/> }
-                    { textEdition && textEdition === 'contact_en' && <CONTACT isEditing='contact_en'/> }
                     { textEdition && textEdition === 'blog_pt' && <BLOG isEditing='blog_pt'/> }
-                    { textEdition && textEdition === 'blog_en' && <BLOG isEditing='blog_en'/> }
 
                 </div>
             }
