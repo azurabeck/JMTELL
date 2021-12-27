61f4ee0fdb4e52dc39f1080d17e72b5e25d9c40f
Rebecca Souza <rebecca@psafe.com>
2021-12-27 19:26:34 -0300

    image preview

--- src/web_pages/pages/products_details/index.js
+++ src/web_pages/pages/products_details/index.js
@@ -1,4 +1,4 @@
-import React , {useEffect} from 'react'
+import React , {useState} from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 import { firestoreConnect } from 'react-redux-firebase'
@@ -13,11 +13,9 @@ const ProductList = (props) => {
     
     const PRODUCTS_DATA = props.products
     const ALL_PRODUCT_DB = props.allProducts
-    useEffect(() => {
-        window.scrollTo(0, 0)
-    });
     
     const RELATED = ALL_PRODUCT_DB && ALL_PRODUCT_DB.filter( item => item.categorie.includes(PRODUCTS_DATA[0].categorie[0])  )
+    const [ showing, handleImageChange ] = useState(0)
     
     const handleFilterDispatch = (e , filter) => {
         e.preventDefault()
@@ -32,7 +30,26 @@ const ProductList = (props) => {
                     return (
                         <div key={index}>
                             <div className="product-main-details">
-                                <div className='product-main-details-img'> <img className="product-img" alt="" src={item.img ? item.img : DEFAULT } /> </div>
+                                <div className='product-main-details-img'> 
+
+                                    <div className='group-image'>
+
+                                        { item.img ? <img className="product-img" alt="" src={ item.img } onClick={() => handleImageChange(0)} /> : null }
+                                        { item.img_1 ? <img className="product-img" alt="" src={item.img_1 } onClick={() => handleImageChange(1)} /> : null }
+                                        { item.img_2 ? <img className="product-img" alt="" src={item.img_2 } onClick={() => handleImageChange(2)} /> : null }
+                                        { item.img_3 ? <img className="product-img" alt="" src={item.img_3 } onClick={() => handleImageChange(3)} /> : null }
+
+                                    </div>
+
+                                    <div className='preview'>
+                                        { showing === 0 && <img className="product-img" alt="" src={item.img ? item.img : DEFAULT } /> }
+                                        { showing === 1 && <img className="product-img" alt="" src={item.img ? item.img_1 : DEFAULT } /> }
+                                        { showing === 2 && <img className="product-img" alt="" src={item.img ? item.img_2 : DEFAULT } /> }
+                                        { showing === 3 && <img className="product-img" alt="" src={item.img ? item.img_3 : DEFAULT } /> }
+
+                                    </div>
+                                
+                                </div>
                                 <div className='product-main-details-desc'>
                                     <div className="pmd-name">{item.name ? item.name : '-' }</div>
                                     <div className="pmd-divisor"></div>
