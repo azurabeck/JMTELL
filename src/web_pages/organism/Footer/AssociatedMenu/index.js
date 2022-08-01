4f3236d7ca3f29dc4bf4ebead7d0bcd6932980e1
rebecca <rebecca@psafe.com>
2022-08-01 13:33:44 -0300

    change link, fix whatsapp

--- src/web_pages/organism/Footer/AssociatedMenu/index.js
+++ src/web_pages/organism/Footer/AssociatedMenu/index.js
@@ -35,11 +35,11 @@ const AssociatedMenu = (props) => {
                         <div className='group-columns'>
                             <div className='column'>
                             <div className='desc'> R. Barão de Melgaço, 880, Cordovil, Rio de Janeiro – RJ 21250-320
-                                <a href="mailto:sac@jmtell.com.br"> sac@jmtell.com.br </a> - (21) 3351-1211 - (21) 3351-8297 </div>
+                                <a href="mailto:sac@jmtell.com.br"> sac@jmtell.com.br </a> - (21) 3351-1211 </div>
                             </div>
                             <div className='column row'>  
-                                <a href="mailto:contato@jmtell.com.br"> <EMAIL /> </a>
-                                <a href='https://api.whatsapp.com/send?l=pt_br&phone=55213351-1211' target='_blank' rel="noreferrer" >
+                                <a href="mailto:sac@jmtell.com.br"> <EMAIL /> </a>
+                                <a href='https://whatsa.me/552133511211' target='_blank' rel="noreferrer" >
                                     <WHATSAPP />
                                 </a>
                                 <BUTTON_STROKE
